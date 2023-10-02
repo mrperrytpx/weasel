98933a68974c4bedcba92aabbecf756a76b1b96d
mrperrytpx <tomislav.siprak1996@gmail.com>
2023-10-02 17:25:17 +0200

    few navbar changes

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { BsMoonStars, BsSun } from "react-icons/bs";
 import { apiInstance } from "../utils/axiosClients";
 import { useQueryClient } from "@tanstack/react-query";
 import WeaselImage from "../assets/weasel.png";
+import { BiLogOut } from "react-icons/bi";
 
 export const Navbar = () => {
     const user = useUser();
@@ -22,7 +23,7 @@ export const Navbar = () => {
     };
 
     return (
-        <div className="dark:bg-periwinkle-900 shadow-periwinkle-100 dark:shadow-periwinkle-950 bg-white p-2 shadow-md">
+        <div className="shadow-periwinkle-100 border-b-periwinkle-300 border-b bg-white p-2 shadow-md dark:border-b-zinc-600 dark:bg-black dark:shadow-zinc-900">
             <div className="mx-auto flex w-full max-w-screen-xl items-center justify-between">
                 <Link to="/">
                     <img
@@ -31,15 +32,34 @@ export const Navbar = () => {
                         alt="A photo of a cartoonish weasel holding a black camera and resting his head on it, looking at the screen."
                     />
                 </Link>
-                <ul className="flex items-center gap-8">
+                <ul className="flex items-center gap-8 font-extrabold">
+                    {user?.data.id && (
+                        <li>
+                            <Link className="uppercase" to="/albums">
+                                Albums
+                            </Link>
+                        </li>
+                    )}
                     <li>
                         {user?.data?.id ? (
-                            <Link to="/profile">Profile</Link>
+                            <Link to="/profile">
+                                <img
+                                    className="aspect-square w-10 rounded-full border-2 border-black dark:border-white"
+                                    src={user?.data?.photos[0].value}
+                                    alt="Your profile image."
+                                />
+                            </Link>
                         ) : (
                             <Link to="/login">Login</Link>
                         )}
                     </li>
-                    <li>{user?.data?.id && <button onClick={logout}>Logout!</button>}</li>
+                    <li>
+                        {user?.data?.id && (
+                            <button className="flex text-center" onClick={logout}>
+                                <BiLogOut size={24} />
+                            </button>
+                        )}
+                    </li>
                     <li>
                         <button className="flex text-center" onClick={toggleTheme}>
                             {darkmode ? <BsSun size={20} /> : <BsMoonStars size={20} />}
--- client/src/components/SocialMediaLoginButton.tsx
+++ client/src/components/SocialMediaLoginButton.tsx
@@ -1,5 +1,7 @@
+type TProviders = "Google" | "Facebook";
+
 type TSocialMediaLoginButtonProps = {
-    provider: string;
+    provider: TProviders;
     signIn: () => void;
     src: string;
 };
@@ -8,13 +10,14 @@ export const SocialMediaLoginButton = ({ provider, signIn, src }: TSocialMediaLo
     return (
         <button
             aria-label={`${provider} sign in.`}
-            className="group flex w-full items-center justify-center gap-2 rounded-lg border-2 bg-white text-sm text-black transition-all duration-75 hover:border-[#4285F4] hover:bg-[#4285F4]"
+            className="flex w-full items-center justify-center gap-2 rounded-lg border-2 bg-white text-sm text-black transition-all duration-75 hover:border-[#4285F4] hover:bg-[#4285F4] hover:text-white"
             onClick={() => signIn()}
+            type="button"
         >
             <div className="rounded-sm bg-white p-2">
                 <img className="aspect-square w-[1.125rem]" src={src} alt={`${provider}'s logo`} />
             </div>
-            <span className="pr-2 font-medium group-hover:text-white">Sign in with {provider}</span>
+            <span className="pr-2 font-medium">Sign in with {provider}</span>
         </button>
     );
 };
