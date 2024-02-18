cef64a2de49875aa6326c10f2127cf5b24cf9758
mrperrytpx <tomislav.siprak1996@gmail.com>
2024-02-18 23:52:16 +0100

    meta tags, vercel.json

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserContextProvider } from "./contexts/UserContext";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -18,13 +18,13 @@ const queryClient = new QueryClient({
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
-        <HashRouter>
+        <BrowserRouter>
             <QueryClientProvider client={queryClient}>
                 <UserContextProvider>
                     <App />
                     <ReactQueryDevtools initialIsOpen={false} />
                 </UserContextProvider>
             </QueryClientProvider>
-        </HashRouter>
+        </BrowserRouter>
     </React.StrictMode>,
 );
